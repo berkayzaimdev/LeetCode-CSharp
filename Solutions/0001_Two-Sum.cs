395f8da3e7ea5b6f72bc1f54f005388296e9c80c
Berkay Zaim <88087972+berkayzaimdev@users.noreply.github.com>
2024-03-17 08:30:04 +0300

    Create 0001_Two-Sum.cs

---
+++ Solutions/0001_Two-Sum.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        Dictionary<int, int> map = new();
+        int complement;
+
+        for(int i=0;i<nums.Length;i++)
+        {
+            complement = target-nums[i];
+            if(map.ContainsKey(complement))
+            {
+                return new int[]{map[complement], i};
+            }
+            map[nums[i]] = i;
+        }
+        return new int[]{};
+    }
+}
